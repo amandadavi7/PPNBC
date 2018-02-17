4302ffca73bc206cf664d9cc11ede5d847f8f846
Anisha Agarwal <anisha9000@gmail.com>
2018-02-16 21:36:14 -0800

    Add Utility files
    Signed-off-by: Anisha Agarwal <anisha9000@gmail.com>

---
+++ src/Utility/Constants.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utility;
+
+/**
+ *
+ * @author anisha
+ */
+public class Constants {
+    public static final String IP ="127.0.0.1";     // Server IP
+    public static final int prime = 11;             // Prime 
+    public static final int binaryPrime = 2;             // Prime for bit calculation
+    public static final int clientCount = 2;        // No of parties computing a function
+    
+    public static final String x = "x";
+    public static final String y = "y";
+    public static final String u = "u";
+    public static final String v = "v";
+    public static final String w = "w";
+    public static final String d = "d";
+    public static final String e = "e";
+    public static final String localD = "localD";
+    public static final String localE = "localE";
+    public static final String product = "product";
+    
+}
---
+++ src/Utility/Logging.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utility;
+
+/**
+ *
+ * @author anisha
+ */
+public class Logging {
+    
+    /**
+     * Log shares from a list
+     * @param variableName
+     * @param shares 
+     */
+    public static void logShares(String variableName, Integer[] shares) {
+        int n = 0;
+        System.out.print(variableName +": ");
+        for(int share:shares) {
+            System.out.print(share + " ");
+        }
+        System.out.println("");
+    }
+    
+    /**
+     * Log a single value
+     * @param variableName
+     * @param value 
+     */
+    public static void logValue(String variableName, int value) {
+        System.out.println(variableName +": "+ value);
+    }
+    
+    /**
+     * Guidelines to use the Party class
+     */
+    public static void partyUsage() {
+        System.out.println("java Party.Party party_port=<port> ti=<TI port> "
+                + "peer_port=<peer port> party_id=<Party Id> "
+                + "values=<secret share>");
+    }
+    
+    /**
+     * Guideline to use the TI class
+     */
+    public static void tiUsage() {
+        System.out.println("java TI <port>");
+    }
+
+}
