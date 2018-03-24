32828fd097a9441ee891f0cc532ab3ce8416ac7b
keerthanaags <g.s.keerthanaa@gmail.com>
2018-03-23 18:23:08 -0700

    modify test model compute into 2 methods - for argmax and others

--- src/Model/TestModel.java
+++ src/Model/TestModel.java
@@ -66,12 +66,13 @@ public class TestModel {
         queueHandlers.submit(senderThread);
         queueHandlers.submit(receiverThread);
     }
-
-    public void compute() {
+    
+    public void callArgMax() {
+        
         ExecutorService es = Executors.newFixedThreadPool(100);
         List<Future<Integer[]>> taskList = new ArrayList<>();
-
         long startTime = System.currentTimeMillis();
+        
         int totalCases = v.size();
         // totalcases number of protocols are submitted to the executorservice
         for (int i = 0; i < totalCases; i++) {
@@ -79,29 +80,13 @@ public class TestModel {
             recQueues.putIfAbsent(i, new LinkedBlockingQueue<>());
             sendQueues.putIfAbsent(i, new LinkedBlockingQueue<>());
 
-            ArgMax multiplicationModule = new ArgMax(v.get(i), tiShares, oneShares, sendQueues.get(i), 
+            ArgMax argmaxModule = new ArgMax(v.get(i), tiShares, oneShares, sendQueues.get(i), 
                 recQueues.get(i), clientId, Constants.binaryPrime, i);
             
             System.out.println("submitted " + i + " argmax");
             
-            /*Comparison multiplicationModule = new Comparison(x.get(i), y.get(i), 
-                    tiShares, oneShares, sendQueues.get(i),
-                recQueues.get(i), clientId, Constants.binaryPrime, i);
-            
-            System.out.println("submitted " + i + " comparison");*/
-            
-            /*DotProduct multiplicationModule = new DotProduct(x.get(i),
-                    y.get(i), tiShares, sendQueues.get(i), recQueues.get(i),
-                    clientId, Constants.prime, i, oneShares);
-
-            System.out.println("Submitted " + i + " dotproduct");*/
-            
-
-            /*Multiplication multiplicationModule = new Multiplication(x.get(i).get(0),y.get(i).get(0),tiShares.get(i)
-                    ,sendQueues.get(i),recQueues.get(i),clientId,Constants.prime,i,oneShares,0);*/
-            
-            Future<Integer[]> multiplicationTask = es.submit(multiplicationModule);
-            taskList.add(multiplicationTask);
+            Future<Integer[]> argmaxTask = es.submit(argmaxModule);
+            taskList.add(argmaxTask);
 
         }
 
@@ -111,8 +96,88 @@ public class TestModel {
             Future<Integer[]> dWorkerResponse = taskList.get(i);
             try {
                 Integer[] result = dWorkerResponse.get();
+                System.out.println("result:" + Arrays.toString(result) + ", #:" + i);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(TestModel.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ExecutionException ex) {
+                Logger.getLogger(TestModel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        receiverThread.setProtocolStatus();
+        senderThread.setProtocolStatus();
+        queueHandlers.shutdown();
+
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println("Avg time duration:" + elapsedTime);
+        
+    }
+    
+    public void callProtocol(int protocolType) {
+        ExecutorService es = Executors.newFixedThreadPool(100);
+        List<Future<Integer>> taskList = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+        int totalCases = x.size();
+        // totalcases number of protocols are submitted to the executorservice
+        switch(protocolType) {
+            case 1:
+                for (int i = 0; i < totalCases; i++) {
+
+                    recQueues.putIfAbsent(i, new LinkedBlockingQueue<>());
+                    sendQueues.putIfAbsent(i, new LinkedBlockingQueue<>());
+                    
+                    Multiplication multiplicationModule = new Multiplication(x.get(i).get(0),y.get(i).get(0),tiShares.get(i)
+                    ,sendQueues.get(i),recQueues.get(i),clientId,Constants.prime,i,oneShares,0);
+                
+                    System.out.println("Submitted " + i + " multiplication");
+                
+                    Future<Integer> multiplicationTask = es.submit(multiplicationModule);
+                    taskList.add(multiplicationTask);
+                }
+                break;
+            case 2:
+                for (int i = 0; i < totalCases; i++) {
+
+                    recQueues.putIfAbsent(i, new LinkedBlockingQueue<>());
+                    sendQueues.putIfAbsent(i, new LinkedBlockingQueue<>());
+                    
+                    DotProduct DPModule = new DotProduct(x.get(i),
+                    y.get(i), tiShares, sendQueues.get(i), recQueues.get(i),
+                    clientId, Constants.prime, i, oneShares);  
+                
+                    System.out.println("Submitted " + i + " dotproduct");
+                    
+                    Future<Integer> DPTask = es.submit(DPModule);
+                    taskList.add(DPTask);
+                }
+                break;
+            case 3:
+                for (int i = 0; i < totalCases; i++) {
+
+                    recQueues.putIfAbsent(i, new LinkedBlockingQueue<>());
+                    sendQueues.putIfAbsent(i, new LinkedBlockingQueue<>());
+                    
+                    Comparison comparisonModule = new Comparison(x.get(i), y.get(i), 
+                    tiShares, oneShares, sendQueues.get(i),
+                    recQueues.get(i), clientId, Constants.binaryPrime, i); 
+                
+                    System.out.println("submitted " + i + " comparison");
+                    
+                    Future<Integer> comparisonTask = es.submit(comparisonModule);
+                    taskList.add(comparisonTask);
+                }
+                break;
+        }
+        
+        es.shutdown();
+
+        for (int i = 0; i < totalCases; i++) {
+            Future<Integer> dWorkerResponse = taskList.get(i);
+            try {
+                Integer result = dWorkerResponse.get();
                 System.out.println("result:" + result + ", #:" + i);
-                //System.out.println("result:" + Arrays.toString(result) + ", #:" + i);
             } catch (InterruptedException ex) {
                 Logger.getLogger(TestModel.class.getName()).log(Level.SEVERE, null, ex);
             } catch (ExecutionException ex) {
@@ -127,5 +192,16 @@ public class TestModel {
         long stopTime = System.currentTimeMillis();
         long elapsedTime = stopTime - startTime;
         System.out.println("Avg time duration:" + elapsedTime);
+    }
+    
+
+    public void compute() {
+        
+        //callArgMax();
+        
+        
+        // pass 1 - multiplication, 2 - dot product and 3 - comparison
+        callProtocol(3);
+        
     }
 }
