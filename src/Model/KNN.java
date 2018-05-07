9af35ed55899538287d32bb40348d4f5f388b942
keerthanaags <g.s.keerthanaa@gmail.com>
2018-05-07 13:12:51 -0700

    compute jaccard distances and append class labels

--- src/Model/KNN.java
+++ src/Model/KNN.java
@@ -6,9 +6,17 @@
 package Model;
 
 import Communication.Message;
+import Protocol.Utility.JaccardDistance;
 import TrustedInitializer.Triple;
+import Utility.Constants;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,6 +27,8 @@ public class KNN extends Model {
     List<List<Integer>> trainingShares;
     List<Integer> testShare;
     List<Integer> classLabels;
+    List<List<Integer>> jaccardDistances;
+    int pid, attrLength;
     
     
     public KNN(int oneShare, BlockingQueue<Message> senderQueue,
@@ -31,10 +41,32 @@ public class KNN extends Model {
         this.trainingShares = trainingShares;
         this.testShare = testShare;
         this.classLabels = classLabels;
+        pid = 0;
+        this.attrLength = testShare.size();
     }
     
     int KNN_Model(){
         //Jaccard Computation
+        ExecutorService es = Executors.newFixedThreadPool(Constants.threadCount);
+        
+        initQueueMap(recQueues, sendQueues, pid);
+        
+        int decTICount = attrLength*2*trainingShares.size();
+        JaccardDistance jdModule = new JaccardDistance(trainingShares, testShare, 
+                oneShares, decimalTiShares.subList(0, decTICount), sendQueues.get(pid), 
+                recQueues.get(pid), clientId, Constants.prime, pid);
+        
+        Future<List<List<Integer>>> jdTask = es.submit(jdModule);
+        
+        try {
+            jaccardDistances = jdTask.get();
+        } catch (InterruptedException | ExecutionException ex) {
+            Logger.getLogger(KNN.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        for(int i=0;i<trainingShares.size();i++) {
+            jaccardDistances.get(i).add(classLabels.get(i));
+        }
         return 0;
     }
     
