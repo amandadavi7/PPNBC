84d1107bffb3ae9f42adb2aefff53393467fa887
Anisha Agarwal <anisha9000@gmail.com>
2018-05-17 15:48:54 -0700

    Run programs in the background using a script
    Signed-off-by: Anisha Agarwal <anisha9000@gmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #TODO this is for lr-test now Do it for damf too 
-unset -x
 
 totaltests=$1
 baseoutputpath="test/pax-test-"
@@ -31,6 +30,7 @@ echo "Copying beta done: $((STOP - START))sec"
 #STEP 3: For each test case run the parties to generate the output
 for (( i=2 ; i <= $totaltests; i++ ))
   do  
+    echo "========================================================================"
     outputpath=$baseoutputpath$i"/"
 
     #start ti
@@ -52,28 +52,29 @@ for (( i=2 ; i <= $totaltests; i++ ))
             yFileName="BETA_"
             xCsvFile="$outputpath$xFileName$j.csv"
             yCsvFile="$outputpath$yFileName$j.csv"
-            echo $xCsvFile
-            echo $yCsvFile
             java -cp build/classes/ Party.Party party_port=$(($startPort+100)) ti=127.0.0.1:3000 ba=127.0.0.1:4000 party_id=$j yCsv="$yCsvFile" xCsv="$xCsvFile" oneShares=0 output=$outputpath model=2 &
 	    pids[$((2+$j))]=$!
-            echo "Started party$j; process id:"$pids[$((2+$j))]
+            echo "Started party $j; process id:"$pids[$((2+$j))]
+            startPort=$(($startPort+100))
 	done
 
+    echo "All processes:"
+    echo ${pids[*]}
     for pid in ${pids[*]}; do
         wait $pid
+    echo "========================================================================"
     done
 
 echo "Party computation done"
+  done
 
 #STEP 4: TODO merge outputfiles and calculate rmse
-
+:'
 for (( i=2 ; i <= $totaltests; i++ ))
   do  
     outputpath=$baseoutputpath$i"/"
     python3 calculate_rmse.py $outputpath $clientPath
   done
-
-  done
-
+'
 
 
