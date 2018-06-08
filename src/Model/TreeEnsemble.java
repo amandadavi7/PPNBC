cc3aa74fe90acda721a67030417a9178dfdc0ef4
keerthanaags <g.s.keerthanaa@gmail.com>
2018-06-07 23:32:56 -0700

    Tree Ensemble class skeleton
    Signed-off-by: keerthanaags <g.s.keerthanaa@gmail.com>

---
+++ src/Model/TreeEnsemble.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Communication.Message;
+import TrustedInitializer.TripleByte;
+import Utility.Logging;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author keerthanaa
+ */
+public class TreeEnsemble extends Model {
+    
+    String csvPath;
+    boolean partyHasTrees;
+    String[] propertyFiles;
+    int noOfTrees;
+    
+    /**
+     * Constructor:
+     * 
+     * Party 1: contains the decision trees
+     * Each tree is stored in a properties file
+     * the metadata is passed to party as "randomforeststored"
+     * contains number of trees and the name of the property files
+     * 
+     * party 2:
+     * csv file,
+     * properties file with randomforestproperties about all the trees
+     *  
+     * @param asymmetricBit
+     * @param senderQueue
+     * @param receiverQueue
+     * @param clientId
+     * @param binaryTriples
+     * @param partyCount
+     * @param args 
+     */
+    public TreeEnsemble(int asymmetricBit, BlockingQueue<Message> senderQueue,
+            BlockingQueue<Message> receiverQueue, int clientId, List<TripleByte> binaryTriples,
+            int partyCount, String[] args) {
+        
+        super(senderQueue, receiverQueue, clientId, asymmetricBit, partyCount);
+        
+        initializeModelVariables(args);
+        
+    }
+    
+     private void initializeModelVariables(String[] args) {
+
+        for (String arg : args) {
+            String[] currInput = arg.split("=");
+            if (currInput.length < 2) {
+                Logging.partyUsage();
+                System.exit(0);
+            }
+
+            String command = currInput[0];
+            String value = currInput[1];
+
+            switch (command) {
+                case "testCsv":
+                    //party has feature vector
+                    csvPath = value;
+                    break;
+                case "randomforeststored":
+                    //party has the tree
+                    partyHasTrees = true;
+                    Properties prop = new Properties();
+                    InputStream input = null;
+                    try {
+                        input = new FileInputStream(value);
+                        prop.load(input);
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(DecisionTreeScoring.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(DecisionTreeScoring.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    noOfTrees = Integer.parseInt(prop.getProperty("treecount"));
+                    String str = prop.getProperty("propertyfiles");
+                    propertyFiles = str.split(",");
+                    break;
+                case "randomforestproperties":
+                    //party has feature vector
+                    partyHasTrees = false;
+                    prop = new Properties();
+                    input = null;
+                    try {
+                        input = new FileInputStream(value);
+                        prop.load(input);
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(DecisionTreeScoring.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(DecisionTreeScoring.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    noOfTrees = Integer.parseInt(prop.getProperty("treecount"));
+                    str = prop.getProperty("propertyfiles");
+                    propertyFiles = str.split(",");
+                    break;
+            }
+        }
+    }
+     
+    public void runTreeEnsembles() {
+        startModelHandlers();
+        
+        long startTime = System.currentTimeMillis();
+        
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+
+        System.out.println("Avg time duration:" + elapsedTime);
+
+        teardownModelHandlers();
+    }
+    
+}
